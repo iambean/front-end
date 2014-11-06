84e3ce6f1ec6f861cbc6fdb6a328e4b0940b8ec4
Bean <mbn.bean@gmail.com>
2014-11-06 20:42:51 +0800

    Update tap1.js

--- researches/tap-event/tap1.js
+++ researches/tap-event/tap1.js
@@ -1,11 +1,32 @@
 
 /**
- * document.querySelectorAll("button").tap(function(e){
+ * Simple implementation.
+ * document.querySelector("button").tap(function(e){
  *    console.log(e, this);
  * });
  */
 
-HTMLElement.prototype.tap = function(){
-  
+HTMLElement.prototype.tap = function(fn, bCapture){
+  var dom = this;
+  if ("ontouchstart" in document) {
+    dom.addEventListener("touchstart", function(e) {
+      var _ = false;
+      
+      dom.addEventListener("touchmove", onMove, bCapture);
+      
+      dom.addEventListener("touchend", function(e) {
+        !_ && (fn.call(this, e));
+        dom.removeEventListener("touchmove", onMove, bCapture);
+        dom.removeEventListener("touchend", arguments.callee, bCapture);
+      }, bCapture);
+      
+      function onMove() {
+          _ = true;
+      }
+    }, bCapture);
+  } else {
+    dom.addEventListener("click", fn, !! bCapture);
+  }
+  return this;
 }
 
