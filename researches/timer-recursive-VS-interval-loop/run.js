99757df4e15acee4d92a95f903151aff75b8c2b7
Bean <mbn.bean@gmail.com>
2014-11-19 15:15:54 +0800

    Update run.js

--- researches/timer-recursive-VS-interval-loop/run.js
+++ researches/timer-recursive-VS-interval-loop/run.js
@@ -1,20 +1,27 @@
 
 var fn = function(){sleep(500);},
   delay = 1000;
+
+var t0 = getTime();
   
 //1)setTimeout mode:
 setTimeout(function(){
+  //console.log("Timeout:"， getTime() - t0);
   fn();
   setTimeout(arguments.callee, delay);
 }, delay);
 
 //1)setInterval mode:
-setInterval(fn, delay);
+setInterval(function(){
+  //console.log("Interval:", getTime() - t0);
+  fn();
+}, delay);
 
 
 //make the engine sleep with the specified time (ms).
 function sleep(time){
   var t0 = getTime();
   while(getTime() - t0 < time){ ; }
-  var getTime = function(){ return Date.now ? Date.now() : +new Date(); }
 }
+
+function getTime(){ return Date.now ? Date.now() : +new Date(); }
