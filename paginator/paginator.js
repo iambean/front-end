16cb07f9a6892d492d53e7ba19fa1983392ce09a
Bean <mbn.bean@gmail.com>
2015-01-05 10:18:22 +0800

    Update paginator.js
    重构：
    1）分离出原型
    2）handle抛出的首要参数改为目标页码，辅助参数为其tag值，更清晰更利于接口编程。

--- paginator/paginator.js
+++ paginator/paginator.js
@@ -1,145 +1,201 @@
 /**
- * Created by beanmao on 2014/10/28.
- * @Title HON分页条组件
- * @desc 当前的分页组件只有一种交互，包含的功能按钮有：第一页first（最后一页last）、上一页prev（下一页next）、页码输入框input、跳转点击按钮go。
- * 		内部维持一个页码值，即当前态(currPage)。
- * 		当处在第一页时，first和prev处于禁用状态，其他可用;
- * 		当处在最后一页时，last和next不可用，其他可用;
- * 		当只有一页数据时 first/last/prev/next都不可用。
- */
-
-;
-/**
+ * Created by beanmao on 2014/12/25.
+ * @title 分页器
+ * @desc 抽象一个分页器，可以指定模板，但是必须遵守指定的规则设置tag，支持自定义组合如下元素：
+ * 		第一页、上一页、下一页、最后一页、跳转到指定页等。
+ * 	所有的跳转函数都抽象到指定的回调函数handle，参数为目标页码等.
+ * @author beanmao@tencent.com
+ * @date 01/04/2015
  * @param options
- * 	@param content {HTML DOM} 容器 required.
+ * 	@param wrapper {HTML DOM} 容器 required.
  * 	@param
- * @returns {{refresh: refreshStatus}}
- * @eg:
-  		$paginater({
-  			//要填充的容器dom
-			content : $("#pagerWrapper").get(0),
-			//初始化时显示的页码
-			currPage : 1,
-			//总页码
-			totalPage : 1,
-			//页码输入框中允许输入的数字长度（位数）
-			maxlength : 4,
-			//每个跳转按钮的事件公共回调。参数key控制各个触发器的名称。（一共有first/prev/next/last/[具体的页码数字]五种值）
-			handle : function(key){
-				var global = HON.videos.global,
-					curr = global.currPage,
-					total = global.totalPage,
-					targetPage = 0;
-				//参数如果是string，则有[first|prev|next|last]4个值，如果是数字，则表示要跳转到具体的页码
-				if(typeof key === "string"){
-					targetPage = {
-						"first" : 1,
-						"prev" 	: curr-1,
-						"next"  : curr+1,
-						"last"  : total
-					}[key];
-				}else{
-					targetPage = key*1;
-				}
-				//合法区间计算
-				targetPage = Math.max(1, Math.min(targetPage, total));
-				HON.videos.loadData({
-					page : targetPage
-				});
-			}
-		});
+ * @eg :
+ var Paginater = require("Paginater"),
+ pager = new Paginator({
+	//要填充的容器dom
+	wrapper : $("#pagerWrapper").get(0),
+	//初始化时显示的页码
+	currPage : 1,
+	//总页码
+	totalPage : 1,
+	//页码输入框中允许输入的数字长度（位数）
+	maxlength : 4,
+	//每个跳转按钮的事件公共回调。参数key控制各个触发器的名称。（一共有first/prev/next/last/[具体的页码数字]五种值）
+	handle : function(targetPage, tag, e){
+		console.log("要跳转到第"+targetPage+"页");
+	}
+});
  */
-window.$paginater = function(options){
-	var htmlTmpl = '<p class="page-turn">\
-						<a tag="first" class="first-page" href="#" title="点击去到第一页" hidefocus="">|&lt;</a><span class="first-page first-page-gray">|&lt;</span><a tag="prev" class="prev-page" href="#" title="上一页" hidefocus="">&lt;</a><span class="prev-page prev-page-gray">&lt;</span><input type="text" maxlength="3"><span tag="pager" class="page-total"></span><a tag="go" href="#" hidefocus="">GO</a><a tag="next" class="next-page" title="下一页" href="#" hidefocus="">&gt;</a><span class="next-page next-page-gray">&gt;</span><a tag="last" class="last-page" href="#" title="点击去到最后一页" hidefocus="">&gt;|</a><span class="last-page last-page-gray">&gt;|</span>\
-					</p>';
-
-	var content = $(options.content);
-
-	content.html(htmlTmpl);
-
-	//几个重点元素
-	var first 	= content.find("[tag='first']"),
-		prev 	= content.find("[tag='prev']"),
-		input 	= content.find(":text"),
-		pager 	= content.find("[tag='pager']"),
-		go 		= content.find("[tag='go']"),
-		next 	= content.find("[tag='next']"),
-		last 	= content.find("[tag='last']");
-
+;
+(function(window){
 	/**
-	 * 点击几个代表相对值的按钮时，将标记值传入回调执行。
+	 * @param options
+	 * @constructor Paginator
 	 */
-	first.add(prev).add(next).add(last).click(function(){
-		var type = $(this).attr("tag");
-		options.handle(type);
-		return false;
-	});
+	function Paginator(options){
+		//先将参数全部挂到对象上。
+		this.options = options;
 
-	/**
-	 * 在页码输入框内回车则用go按钮的click事件代理执行。
-	 */
-	input.keypress(function(e){
-		var kc = e.keyCode;
-		if(kc === 13){
-			go.click();
-		}else if(/\D/.test(String.fromCharCode(kc))){
+		var htmlTmpl = options.htmlTmpl;
+		if(!htmlTmpl){
+			htmlTmpl = '<p class="page-turn">\
+					<a tag="first" class="first-page" href="#" title="点击去到第一页" hidefocus="">|&lt;</a>\
+					<a tag="prev" class="prev-page" href="#" title="上一页" hidefocus="">&lt;</a>\
+					<input type="text" maxlength="3">\
+					<span class="page-total">/<span tag="total" ></span></span>\
+					<a tag="go" href="#" hidefocus="">GO</a>\
+					<a tag="next" class="next-page" title="下一页" href="#" hidefocus="">&gt;</a>\
+					<a tag="last" class="last-page" href="#" title="点击去到最后一页" hidefocus="">&gt;|</a>\
+				</p>';
+		}
+
+		var wrapper = $(options.wrapper);
+
+		wrapper.html(htmlTmpl);
+
+		this.wrapper = wrapper;
+		this.currPage = options.currPage * 1;
+		this.totalPage = options.totalPage * 1;
+
+		//几个关键元素:
+		this.elems = {
+			first 	: wrapper.find("[tag='first']"),
+			prev 	: wrapper.find("[tag='prev']"),
+			input 	: wrapper.find(":text"),
+			curr	: wrapper.find("[tag='curr']"),//input和curr应该只允许一个，input是可以输入页码型，curr是不可输入型。
+			total 	: wrapper.find("[tag='total']"),
+			go 	: wrapper.find("[tag='go']"),
+			next 	: wrapper.find("[tag='next']"),
+			last 	: wrapper.find("[tag='last']")
+		};
+
+		var that 	= this,
+			elems 	= this.elems,
+			first 	= elems.first,
+			prev 	= elems.prev,
+			input 	= elems.input,
+			total 	= elems.total,
+			go 	= elems.go,
+			next 	= elems.next,
+			last 	= elems.last;
+
+		/**
+		 * 点击几个代表相对值的按钮时，将标记值传入回调执行。
+		 */
+		first.add(prev).add(next).add(last).click(function(e){
+			var tag = $(this).attr("tag");
+			if(typeof options.handle === "function"){
+				var targetNum = {
+					"first" : 1,
+					"prev" 	: that.currPage - 1,
+					"next"  : that.currPage + 1,
+					"last"  : that.totalPage
+				}[tag];
+				//合法区间计算
+				targetNum = Math.max(1, Math.min(targetNum, that.totalPage));
+				if(targetNum !== that.currPage){
+					options.handle.call(that, targetNum, tag, e);
+				}
+			}
 			return false;
+		});
+
+		/**
+		 * 在页码输入框内回车则用go按钮的click事件代理执行。
+		 */
+		if(input.length > 0){
+			input.keypress(function(e){
+				var kc = e.keyCode;
+				if(kc === 13){
+					go.click();
+				}else if(/\D/.test(String.fromCharCode(kc))){
+					return false;
+				}
+			});
+			//设置maxlength:
+			var maxlen = parseInt(options.maxlength);
+			if(!isNaN(maxlen) && maxlen>0){
+				input.attr("maxlength", maxlen);
+			}
 		}
-	});
+
+		/**
+		 * 点击跳转时先检查一下页码输入框的值，如果非法则默认第一页，否则解出页码值。
+		 * 把页码值传入给回调函数,这里为了保证执行侧有一定的区分度，页码值严格限制为number而不能传入string。
+		 */
+		if(go.length > 0){
+			go.click(function(e){
+				var pageNum = $.trim(input.val());
+				if(/^\d+$/.test(pageNum)){
+					pageNum *= 1;
+				}else{
+					pageNum = 1;
+				}
+				options.handle.call(that, pageNum, "go", e);
+				return false;
+			});
+		}
+
+		//初始化状态
+		this.refresh(options.currPage, options.totalPage);
+	}
 
 	/**
-	 * 点击跳转时先检查一下页码输入框的值，如果非法则默认第一页，否则解出页码值。
-	 * 把页码值传入给回调函数,这里为了保证执行侧有一定的区分度，页码值严格限制为number而不能传入string。
+	 * prototype属性定义。
 	 */
-	go.click(function(){
-		var pageNum = $.trim(input.val());
-		if(/^\d+$/.test(pageNum)){
-			pageNum *= 1;
-		}else{
-			pageNum = 1;
-		}
-		options.handle(pageNum);
-		return false;
-	});
+	Paginator.prototype = {
+		//初始化的参数挂载
+		options : null,
+		//容器
+		wrapper : null,
+		//几个关键dom元素
+		elems	: null,
+		//当前页码
+		currPage : 0,
+		//总页码
+		totalPage : 0,
+		//刷新状态的方法
+		refresh : function(currPage, totalPage){
+			if(currPage < 1 || totalPage < 1 || currPage > totalPage){
+				return false;
+			}
+			this.currPage = currPage * 1;
+			this.totalPage = totalPage * 1;
 
-	//初始化状态
-	refreshStatus({
-		curr : options.currPage,
-		total : options.totalPage,
-		maxlength : options.maxlength
-	});
+			//doms:
+			var elems 	= this.elems,
+				input 	= elems.input,
+				curr 	= elems.curr,
+				total 	= elems.total;
 
-	/**
-	 * @param curr 当前页码
-	 * @param total 总页数
-	 * 基本规则：curr、total必须是正整数，并且curr必须小于或等于total.
-	 */
-	function refreshStatus(options){
-		var curr = options.curr* 1,
-			total = options.total*1;
-		//先将所有按钮层级的元素全部都显示，后面再针对性的隐藏
-		content.children().children().show();
-		input.val(curr);
-		//可以通过参数设置输入框的maxlength.
-		var maxlen = parseInt(options.maxlength);
-		if(!isNaN(maxlen) && maxlen>0){
-			input.attr("maxlength", maxlen);
+			if(input.length > 0){
+				input.val(this.currPage);
+			}
+
+			if(curr.length > 0){
+				curr.text(this.currPage);
+			}
+
+			total.text(this.totalPage);
+
+			if(typeof this.options.onRefresh === "function"){
+				this.options.onRefresh.call(this);
+			}
 		}
-		pager.text("/" + total);
-		if(curr === 1){
-			first.add(prev).hide();
-		}else{
-			first.next().add(prev.next()).hide();
-		}
-		if(curr === total){
-			last.add(next).hide();
-		}else{
-			last.next().add(next.next()).hide();
-		}
+	};
+
+	//构造器还原
+	Paginator.prototype.constructor = Paginator;
+
+	//1)依赖jQuery， 2)支持amd/cmd规范。
+	if(!window.jQuery){
+		return window.console || window.console.error || window.console.error("jquery required!");
 	}
-	//抛出refresh方法
-	return {
-		refresh : refreshStatus
+	if(typeof define === "function" && (define.amd || define.cmd)){
+		define(function (require, exports, module) {
+			return Paginator;
+		});
+	}else{
+		window.Paginator = Paginator;
 	}
-};
+})(window);
