c217b0623fa4b117b921c27d0bdb640544355e41
Bean <mbn.bean@gmail.com>
2015-02-28 16:48:16 +0800

    Update slide-direction.js

--- researches/slide-direction.js
+++ researches/slide-direction.js
@@ -1,8 +1,9 @@
 /**
  * @title: 判断用户的滑动方向是水平还是垂直方向的。
  * @desc: 取起始点和第一次move的点坐标计算X轴夹角，
- *      若是大于指定的临界值crossover(默认20°)则认为是垂直方向，否则是水平方向的。
- *      这里只针对触摸设备的单指滑动操作。
+ *      1) 若是大于指定的临界值crossover(默认20°)则认为是垂直方向，否则是水平方向的。
+ *      2) 这里只针对触摸设备的单指滑动操作。
+ *      3) 给回调的两个参数：第一个flag表示[手指滑动的方向]，有left/right/bottom/top四个值;
  */
 function slideDirection(cfg) {
     var dom = cfg.dom,//判断滑动方向的容器
@@ -30,7 +31,14 @@ function slideDirection(cfg) {
             var deltaX = e.pageX - point.x,
                 deltaY = e.pageY - point.y,
                 degree = (Math.atan(Math.abs(deltaY / deltaX)) * 180) / Math.PI;
-            callback(degree < crossover ? "h" : "v");
+            //callback(degree < crossover ? "h" : "v");
+            var flag = "";
+            if(degree < crossover){
+                flag = deltaX > 0 ? "right" : "left";
+            }else{
+                flag = deltaY > 0 ? "bottom" : "top";
+            }
+            callback(flag, degree);
             //每次完整的滑动流程中，保证move只执行一次。
             dom.removeEventListener("touchmove", _move, true);
         }
